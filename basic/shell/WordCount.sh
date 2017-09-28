caa0148f88c71ec8ec6972421f83efb3d6b6021a
dreamcat <solitudesword@gmail.com>
2017-09-28 16:27:14 +0800

    WordCount对比算子
    WordCount用了两种算子，写了对比和结论

--- basic/shell/WordCount.sh
+++ basic/shell/WordCount.sh
@@ -11,12 +11,20 @@ ProjectRootDir=`readlink -f \`dirname $0\`/../../`
 # 导入变量
 source ${ProjectRootDir}/shell/baseEnv.sh
 
-# 提交作业
-${SparkSubmitPath} \
-    --master ${SparkMaster} \
-    --executor-memory 1g \
-    --executor-cores 2 \
-    --total-executor-cores 6 \
-    --driver-class-path ${ProjectRootDir}/base/target/spark.small.demo.base-1.0.jar \
-    --class cat.dream.miaomiao.enter.WordCount \
-    ${ProjectRootDir}/basic/target/spark.small.demo.basic-1.0.jar
+# 删除前面的输出
+${HadoopPath} fs -rm -f -r /output
+
+testFns=(reduce group)
+
+for testFn in ${testFns[*]}; do
+    # 提交作业
+    ${SparkSubmitPath} \
+        --master ${SparkMaster} \
+        --executor-memory 1g \
+        --executor-cores 2 \
+        --total-executor-cores 6 \
+        --driver-class-path ${ProjectRootDir}/base/target/spark.small.demo.base-1.0.jar \
+        --class cat.dream.miaomiao.enter.WordCount \
+        ${ProjectRootDir}/basic/target/spark.small.demo.basic-1.0.jar ${testFn}
+done
+
--- basic/src/main/scala/cat/dream/miaomiao/enter/WordCount.scala
+++ basic/src/main/scala/cat/dream/miaomiao/enter/WordCount.scala
@@ -4,6 +4,7 @@ import java.io.{BufferedInputStream, FileInputStream}
 import java.util.Properties
 
 import cat.dream.common.BaseConfig
+import org.apache.log4j.Logger
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -15,9 +16,12 @@ object WordCount {
     /**
       * 入口函数
       *
-      * @param args 参数列表，无
+      * @param args 参数列表
+      *             0 : 使用方法 支持reduce group
       */
     def main(args: Array[String]): Unit = {
+        // 只显示错误日志
+        Logger.getLogger("org").setLevel(org.apache.log4j.Level.ERROR)
         // 初始化
         val projectRoot = BaseConfig.getProjectRootDir // 项目根目录
         // 配置读取器
@@ -26,14 +30,41 @@ object WordCount {
         coreConf.load(new BufferedInputStream(new FileInputStream(s"$projectRoot/conf/hdfs.properties")))
         // 读取hdfs地址
         val hdfsMaster = coreConf.getProperty("hdfs.master")
+        (if (args.length > 0) args(0) else "reduce") match {
+            case "reduce" => byReduce(hdfsMaster)
+            case "group" => byGroup(hdfsMaster)
+            case _ => Logger.getLogger("org").error("invalid type")
+        }
+    }
 
-        val sc = new SparkContext(new SparkConf().setAppName("WordCount"))
+    /**
+      * 通过groupBy来操作
+      * @param hdfsMaster hdfs地址
+      */
+    def byGroup(hdfsMaster: String): Unit = {
+        val sc = new SparkContext(new SparkConf().setAppName("WordCount-group"))
+        sc.textFile(s"hdfs://$hdfsMaster/word.txt")
+            .flatMap(_.split(" "))
+            .groupBy(_.toString)
+            .map { case (word, it) => s"$word," + it.size.toString }
+            .saveAsTextFile(s"hdfs://$hdfsMaster/output/group")
+
+        // 结束清理
+        sc.stop() // 关闭spark上下文
+    }
+
+    /**
+      * 通过reduceByKey来操作
+      * @param hdfsMaster hdfs地址
+      */
+    def byReduce(hdfsMaster: String): Unit = {
+        val sc = new SparkContext(new SparkConf().setAppName("WordCount-reduceByKey"))
         sc.textFile(s"hdfs://$hdfsMaster/word.txt")
             .flatMap(_.split(" "))
             .map((_, 1L))
             .reduceByKey(_ + _)
             .map { case (word, cnt) => s"$word,$cnt" }
-            .saveAsTextFile(s"hdfs://$hdfsMaster/output")
+            .saveAsTextFile(s"hdfs://$hdfsMaster/output/reduce")
 
         // 结束清理
         sc.stop() // 关闭spark上下文
